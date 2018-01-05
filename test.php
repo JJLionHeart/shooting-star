08c57bd3c0dcf374b6feb8cdb7f6a70840ff9334
Ghostphoenix <jj.lopezjaimez@gmail.com>
2018-01-05 17:16:52 -0600

    scok

--- test.php
+++ test.php
@@ -1,12 +1,11 @@
 <?php
-/*
 $archivo = fopen("text.txt", "a+");
 $text = $_GET["texto"];
 echo $text;
 fwrite($archivo, $text);
 fclose($archivo);
 echo "done";
- */
+/*
 $sock = socket_create_listen(12345);
 socket_getsockname($sock, $addr, $port);
 echo "listening on $addr:$port";
@@ -14,4 +13,5 @@ $c = socket_accept($sock);
 socket_getpeername($c, $raddr, $rport);
 print "Received connection from $raddr:$rport\n";
 socket_close($sock);
+ */
 ?>
