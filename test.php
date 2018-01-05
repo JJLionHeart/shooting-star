a9eb201990ad1ba399fe6a4d81556b8055db562c
Ghostphoenix <jj.lopezjaimez@gmail.com>
2018-01-05 16:51:43 -0600

    test2

--- test.php
+++ test.php
@@ -1,13 +1,8 @@
 <?php
-/*
 $archivo = fopen("text.txt", "a+");
 $text = $_GET["texto"];
 echo $text;
 fwrite($archivo, $text);
 fclose($archivo);
 echo "done";
- */
-$name = $_POST["name"];
-$response = array("text"=>"Hello " . $name);
-echo json_encode($response);
 ?>
