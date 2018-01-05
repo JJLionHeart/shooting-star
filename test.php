960b74e2bd4d7c0a613038a93d04d68706db3823
Ghostphoenix <jj.lopezjaimez@gmail.com>
2018-01-05 17:01:24 -0600

    sockets

--- test.php
+++ test.php
@@ -1,8 +1,15 @@
 <?php
+/*
 $archivo = fopen("text.txt", "a+");
 $text = $_GET["texto"];
 echo $text;
 fwrite($archivo, $text);
 fclose($archivo);
 echo "done";
+ */
+$sock = socket_create_listen(0);
+socket_getsockname($sock, $addr, $port);
+echo $port;
+
+socket_close($sock);
 ?>
